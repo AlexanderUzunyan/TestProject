35497ccca7e01ee0d8b752b0f467d4b116aeb107
Sergey Uzunyan <serguei@nicadd.niu.edu>
2019-06-30 12:47:29 -0500

    minor customization

--- sutest/dsw.sh
+++ sutest/dsw.sh
@@ -8,3 +8,7 @@ export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
 export CPATH=$ROOTSYS/include:$CPATH:$LD_LIBRARY
 #
+if [ "0$(echo $LD_LIBRARY_PATH | grep $PWD/lib)" == "0" ]; then
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib  
+export PATH=$PATH:$PWD/bin  
+fi
