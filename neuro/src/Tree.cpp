4b717617b9a756c8849fcead345074f99dfa241a
[AlexanderUzunyan] <[alexandruzunyan@gmail.com]>
2019-09-22 11:35:17 -0500

    make 2 files to create

--- neuro/src/Tree.cpp
+++ neuro/src/Tree.cpp
@@ -16,7 +16,11 @@ tree_pilot::tree_pilot(int _debug){
 //======================================
 void tree_pilot::file_dat_open(){
 
-  file = fopen("neuro.dat", "r");
+  file = fopen("neuro_signal.dat", "r");
+
+  file1 = fopen("neuro_back.dat", "r");
+
+
    
   if (debug == 2){
   cout << ":File_dat_open: " << endl;
@@ -30,7 +34,9 @@ void tree_pilot::file_dat_open(){
 //======================================
 void tree_pilot::file_root_open(){
 
-   f = new TFile("neuro.root", "RECREATE");
+   f = new TFile("neuro_signal.root", "RECREATE");
+
+   f1 = new TFile("neuro_back.root", "RECREATE");
    
    if (debug == 2){
      cout << ":file_root_open: " << endl;
@@ -46,12 +52,15 @@ void tree_pilot::file_root_open(){
 void tree_pilot::fill_tree(){
 
   tree = new TTree("T", "neuro data from ascii file");
+  tree1 = new TTree("T", "neuro data from ascii file");
+  
 
    if (tree != NULL){
      cout << "Tree is created" << endl;
    }
 
    tree->Branch("Pilot Death propobility",&pilot_param,"nr/I:age/I:e_color/I:sex/I:height/D:body_weight/D:death_propability/D");
+   tree1->Branch("Pilot Death propobility",&pilot_param,"nr/I:age/I:e_color/I:sex/I:height/D:body_weight/D:death_propability/D");
    
    cout << "Branch is created" << endl;
    //  pilot_param.nr=0;
@@ -72,13 +81,25 @@ void tree_pilot::fill_tree(){
      tree -> Fill();
   }
 
+    while(fgets(line,49,file1)){
+      sscanf(&line[0],"%lf %lf %d %d %d %lf",&pilot_param.height,&pilot_param.body_weight,&pilot_param.age,&pilot_param.eyes_color,&pilot_param.sex,&pilot_param.death_propability);
+      cout << "pilot_param.death_propability: " << pilot_param.death_propability << endl;
+      pilot_param.nr++;    
+//     //sscanf(&line[0],"%f%f%d",&pilot_param.height,&pilot_param.body_weight,&pilot_param.age);
+//     //sscanf(&line[40],"%d%d%f",&pilot_param.eyes_color,&pilot_param.male,&pilot_param.death_propability);
+     tree1 -> Fill();
+  }
+
+
   //  if (debug == 2){
   //  cout << "Tree is filled: " << endl;
     
    cout << "passed fill " << endl;
    tree->Write();
+   tree1->Write();
    cout << "passed write " << endl;
    f->Close();
+   f1->Close();
    cout << "passed close " << endl;
 
   //}
